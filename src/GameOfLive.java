a230018a23845d8ad62d675736f2ba87457ae82b
VictoriaDiro <victoriadiro01@gmail.com>
2019-08-03 12:12:37 +0200

    Add improvements

--- src/GameOfLive.java
+++ src/GameOfLive.java
@@ -1,114 +1,90 @@
 import java.util.Random;
 
-import static java.lang.reflect.Array.set;
-
 public class GameOfLive {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        // Declaración de  tablero inicial
         int x = 10;
         int y = 10;
 
-        Random rnd = new Random();
+        boolean[][] theMatrix = firstGeneration(x, y);
 
-        int[][] matrix = new int[x][y];
-
-        System.out.println("");
-        System.out.println("1st Generation");
-        System.out.println("==============");
-
-        for (int i = 0; i < matrix.length; i++) {
-
-            for (int j = 0; j < matrix[i].length; j++) {
-                matrix[i][j] = rnd.nextInt(10);
-
-                if ((matrix[i][j]) % 2 == 0) {
-                    System.out.print('*');
-                } else {
-                    System.out.print(' ');
-                }
-            }
-            System.out.println("");
-
-            // nextGeneration(matrix, M, N);
-            // Thread.sleep(100);
+        for (int i = 0; i < 500; i++) {
+           theMatrix = nextGeneration(theMatrix);
+           Thread.sleep(200);
         }
     }
 
-    static void nextGeneration ( int createMatrix[][], int M, int N) {
+    static boolean[][] firstGeneration(int x, int y) {
 
-        int[][] future = new int[M][N];
+        boolean[][] matrix = new boolean[x][y];
+
+        Random rnd = new Random();
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+
+                matrix[i][j] = rnd.nextInt() < 0.5;
+
+                printMatrix(matrix[i][j]);
+            }
+            System.out.println("");
+            
+        }
+        return matrix;
+    }
+
+    static boolean[][] nextGeneration(boolean[][] theMatrix) {
+
+        boolean[][] nextMatrix = new boolean[theMatrix.length][theMatrix[0].length];
+
+        for (int i = 0; i < theMatrix.length; i++) {
+            for (int j = 0; j < theMatrix[i].length; j++) {
+
+                int aliveNeighbours = 0;
+
+                for (int iAux = i - 1; iAux <= i + 1; iAux++) {
+                    if (iAux > 0 && iAux < theMatrix.length) {
+
+                        for (int jAux = j - 1; jAux <= j + 1; jAux++) {
+                            if (jAux > 0 && jAux < theMatrix[i].length) {
+
+                                if (iAux != i || jAux != j) {
+                                    if (theMatrix[iAux][jAux]) {
+                                        aliveNeighbours += 1;
+                                    }
+                                }
+
+                                if (theMatrix[i][j]) {
+                                    if (aliveNeighbours == 2 || aliveNeighbours == 3) {
+                                        nextMatrix[i][j] = true;
+                                    } else {
+                                        nextMatrix[i][j] = false;
+                                    }
+                                } else {
+                                    if (aliveNeighbours == 3) {
+                                        nextMatrix[i][j] = true;
+                                    } else {
+                                        nextMatrix[i][j] = false;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                printMatrix(nextMatrix[i][j]);
+            }
+            System.out.println("");
+        }
+        return nextMatrix;
+    }
 
 
-
+    static void printMatrix(boolean value) {
+        if (value) {
+            System.out.print('*');
+        } else {
+            System.out.print(' ');
+        }
     }
 }
-
-
-/*
-
----> MAIN
-- tratar de crear todo con booleano
-- declarar tablero inicial
-- asignarle jugar al juego
-- crear juego
-- crear nuevo tablero
-- comprobar los vecinos
-- comprobar numero de vecinos
-- devolver celulas vivas
-
-
- */
-
-
-
-/*
-
-for (int l = 1; l < M; l++) {
-  for (int m = 0; m < N -1; m++) {
-
-      int aliveNeighbours = 0;
-
-      for (int i = -1; i <= 1; i++) {
-          for (int j = -1; j <= 1; j++) {
-              int row = l + i;
-              int col = m + i;
-
-              //aliveNeighbours += createMatrix[l + i][m + j];
-              //aliveNeighbours -= createMatrix[l][m];
-
-              // RULES OF LIFE
-
-              private static boolean canAccess(createMatrix, col) {
-
-                  if(row !=0 && col != 0) {
-                      aliveNeighbours += createMatrix[row][col];
-                  }
-              }
-
-              if(canAccess(createMatrix, row) && canAccess(createMatrix[row], col) && i != 0 && i != 0){
-                  aliveNeighbours += createMatrix[row][col];
-              }
-
-              if ((createMatrix[l][m] == 1) && (aliveNeighbours < 2)) future[l][m] = 0;
-              else if ((createMatrix[l][m] == 1) && (aliveNeighbours > 3)) future[l][m] = 0;
-              else if ((createMatrix[l][m] == 1) && (aliveNeighbours == 3)) future[l][m] = 1;
-              else future[l][m] = createMatrix[l][m];
-          }
-      }
-
-      System.out.println("New Generation");
-
-      for (int i = 0; i < M; i++) {
-          for (int j = 0; j < N; j++) {
-              if (future[i][j] == 0) System.out.print('.');
-              else System.out.print('*');
-          }
-          System.out.println();
-      }
-  }
-}
-}
-
- */
